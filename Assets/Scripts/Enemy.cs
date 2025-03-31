a2a7a285010c36b804c62c5a08e6cd68d133475d
ParkerRailton <parker.railton@gmail.com>
2025-03-30 21:24:15 -0400

    combat changes

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using System;
+using UnityEngine.UI;
+using System.Reflection;
 
 [System.Serializable]
 public class Enemy
@@ -11,9 +13,10 @@ public class Enemy
     public int hp = 0;
     public int hPThresh;
     public int attackStrength;
+    public Sprite sprite;
 
     public int takeDamage(Insult insult)
-    {  
+    {
         int damage = Math.Max(insult.cool - this.cool, 0) + Math.Max(insult.strength - this.strength, 0) + Math.Max(insult.wit - this.wit, 0);
         hp += damage;
         return damage;
@@ -32,6 +35,19 @@ public class Enemy
         wit = _wit;
         hPThresh = _hpThresh;
         attackStrength = _attackStrength;
+
+
+    }
+
+    public Enemy(string _enemyName, int _cool, int _strength, int _wit, int _hpThresh, int _attackStrength, Sprite _sprite)
+    {
+        enemyName = _enemyName;
+        cool = _cool;
+        strength = _strength;
+        wit = _wit;
+        hPThresh = _hpThresh;
+        attackStrength = _attackStrength;
+        sprite = _sprite;
     }
 
 }
